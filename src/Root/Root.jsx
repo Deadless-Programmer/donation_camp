e5f3679be39dc9d85dbb0285a8c5cc0c2f999092
Deadless-Programmer <webtuhin@gmail.com>
2024-03-18 19:12:38 +0600

    add loading

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Home from "../components/Home/Home";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, useNavigation } from "react-router-dom";
 import NavBar from "../components/NavBar/NavBar";
 import Footer from "../components/Footer/Footer";
 
 const Root = () => {
+  const navigation = useNavigation();
   return (
     <div>
       <NavBar></NavBar>
-      <Outlet></Outlet>
+      {navigation.state === "loading" ? 'Loading data plz wait':<Outlet></Outlet>}
       <Footer></Footer>
     </div>
   );
